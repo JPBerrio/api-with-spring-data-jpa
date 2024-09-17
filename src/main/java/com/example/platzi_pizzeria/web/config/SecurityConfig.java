476d20c6fcc2c77aa2739ffc0f657b6af42f8f1a
juanbebr <jberrio@sura.com.co>
2024-09-16 22:18:10 -0500

    Adding spring security

---
+++ src/main/java/com/example/platzi_pizzeria/web/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.platzi_pizzeria.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(customizeRequests -> customizeRequests
+                        .anyRequest()
+                        .authenticated()
+                ).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
