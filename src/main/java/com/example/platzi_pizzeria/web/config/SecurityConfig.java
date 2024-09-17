12864251a35b33a666d2bc76be9d633e7695ada4
juanbebr <jberrio@sura.com.co>
2024-09-17 09:46:16 -0500

    Add request matchers

--- src/main/java/com/example/platzi_pizzeria/web/config/SecurityConfig.java
+++ src/main/java/com/example/platzi_pizzeria/web/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.platzi_pizzeria.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -16,9 +17,12 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(customizeRequests -> customizeRequests
+                        .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                        .requestMatchers(HttpMethod.PUT).denyAll()
                         .anyRequest()
                         .authenticated()
-                ).httpBasic(Customizer.withDefaults());
+                )
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 }
