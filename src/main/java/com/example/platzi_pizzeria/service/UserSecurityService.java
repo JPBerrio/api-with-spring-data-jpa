fb5216d274da81247cd343bd300749264e92fb6e
juanbebr <jberrio@sura.com.co>
2024-09-17 20:22:49 -0500

    creating and assing roles

--- src/main/java/com/example/platzi_pizzeria/service/UserSecurityService.java
+++ src/main/java/com/example/platzi_pizzeria/service/UserSecurityService.java
@@ -1,6 +1,7 @@
 package com.example.platzi_pizzeria.service;
 
 import com.example.platzi_pizzeria.persistence.entity.UserEntity;
+import com.example.platzi_pizzeria.persistence.entity.UserRoleEntity;
 import com.example.platzi_pizzeria.persistence.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
@@ -23,10 +24,13 @@ public class UserSecurityService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserEntity userEntity = this.userRepository.findById(username)
                 .orElseThrow(() -> new UsernameNotFoundException("user " + username + " not found"));
+
+        String[] roles = userEntity.getRoles().stream().map(UserRoleEntity::getRole).toArray(String[]::new);
+
         return User.builder()
                 .username(userEntity.getUsername())
                 .password(userEntity.getPassword())
-                .roles("ADMIN")
+                .roles(roles)
                 .accountLocked(userEntity.getLocked())
                 .disabled(userEntity.getDisabled())
                 .build();
