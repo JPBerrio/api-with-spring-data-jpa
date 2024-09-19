e4ad2cd714731e8be4789726ad02b95205c9150f
juanbebr <jberrio@sura.com.co>
2024-09-18 22:15:37 -0500

    Add audit

--- src/main/java/com/example/platzi_pizzeria/persistence/entity/PizzaEntity.java
+++ src/main/java/com/example/platzi_pizzeria/persistence/entity/PizzaEntity.java
@@ -1,14 +1,12 @@
 package com.example.platzi_pizzeria.persistence.entity;
 
+import com.example.platzi_pizzeria.persistence.audit.AuditableEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "pizza")
@@ -16,7 +14,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
-public class PizzaEntity extends AuditableEntity{
+public class PizzaEntity extends AuditableEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_pizza", nullable = false)
